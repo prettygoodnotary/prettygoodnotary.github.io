bf9a28cbb13dd283f0ad8521cff23417b3477d66
emisaki <emisaki@test1212s-MacBook-Air.local>
2014-05-09 15:57:28 -0700

    Changed lone signing wording

--- Pretty_Good_Notary_files/Pretty_Good_Notary.js
+++ Pretty_Good_Notary_files/Pretty_Good_Notary.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20140508
+// Created by iWeb 3.0.4 local-build-20140509
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
